6a945a9b4c40cb0ba08130080b3505a304cf4b67
Randall Nagy <randall.nagy@soft9000.com>
2022-03-13 05:51:39 -0400

    The next mission.

---
+++ com/soft9000/M1000/A00100/MainParams.java
@@ -0,0 +1,10 @@
+package com.soft9000.M1000.A00100;
+
+public class MainParams {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("Got: " + arg + " ...!");
+        }
+    }
+}
